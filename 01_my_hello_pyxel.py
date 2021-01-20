eed6b4c1474827263a46faeb875be86eb18c3795
Kenta Morita <kenrur339@gmail.com>
2021-01-20 21:08:23 +0900

    Day2-mouse

--- 01_my_hello_pyxel.py
+++ 01_my_hello_pyxel.py
@@ -7,12 +7,17 @@ CAT_W = 16
 
 class App:
 	def __init__(self):
-		pyxel.init(WINDOW_W, WINDOW_H, caption="Hello Pyxel")
-		pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
-		pyxel.image(1).load(0, 0, "assets/cat_16x16.png")
+		self.IMG_ID0 = 0
+		self.IMG_ID1 = 1
+		# self.IMG_ID2 = 2
+		self.IMG_ID0_X = 60
+		self.IMG_ID0_Y = 65
 
-		pyxel.mouse(True)
+		pyxel.init(WINDOW_W, WINDOW_H, caption="Cat Game")
+		pyxel.image(self.IMG_ID0).load(0, 0, "assets/pyxel_logo_38x16.png")
+		pyxel.image(self.IMG_ID1).load(0, 0, "assets/cat_16x16.png")
 
+		# pyxel.mouse(True)
 		pyxel.run(self.update, self.draw)
 
 	def update(self):
@@ -21,7 +26,20 @@ class App:
 	
 	def draw(self):
 		pyxel.cls(0)
-		pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
-		pyxel.blt(61, 66, 0, 0, 0, 38, 16)
-		pyxel.blt(75, 45, 1, 0, 0, CAT_W, CAT_H, 13)
+		
+		pyxel.text(55, 40, "Are you Kururu?", pyxel.frame_count % 16)
+		
+		pyxel.blt(self.IMG_ID0_X, self.IMG_ID0_Y, self.IMG_ID0, 0, 0, 38, 16)
+
+		self.cat()
+
+	def cat(self):
+		x = pyxel.mouse_x
+		y = pyxel.mouse_y
+
+		if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
+			pyxel.blt(x, y, self.IMG_ID1, 0, 0, -CAT_W, CAT_H, 13)
+		else:
+			pyxel.blt(x, y, self.IMG_ID1, 0, 0, CAT_W, CAT_H, 13)
+
 App()
